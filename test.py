bf1f92e9fb1e6e90231d84e6188c7b0cd2a51819
Kelly Rich <103385007+kbob77@users.noreply.github.com>
2023-01-09 19:44:37 -0800

    added message boxes to write dinners to text file

--- test.py
+++ test.py
@@ -1,26 +1,34 @@
 from tkinter import *
-from tkinter import messagebox
+import tkinter.messagebox
+import customtkinter
 import random
+from tkinter import filedialog
+
+customtkinter.set_appearance_mode("light")  
+customtkinter.set_default_color_theme("blue") 
 
 t = (r'F:\OneDrive\Documents\GitHub\FirstClassProject\test.txt')
-root = Tk()
-root.geometry("1100x333")
-
+root = customtkinter.CTk()
+root.geometry("1300x333")
 root.title("Dinner Program")
 
+def quit_program():
+    root.quit()
+
 def randomDinner():
-  # with open('test.txt') as d:
-  #   rdin = [line.strip() for line in d]
-  rdin = open('dinners.txt').read().splitlines()
-  # result = random.choice(rdin)
+  rdin = open('test.txt').read().splitlines()
   index = random.randint(0, len(rdin)-1)
-  print(index)
   result = rdin[index]
   
-  # random_dinner = Label(root, text=result)
-  # random_dinner.grid(row=24)
+  random_dinner = Label(root, text=result)
+  random_dinner.grid(row=24, columnspan=3)
   
-  
+  add_to_file = tkinter.messagebox.askyesno('Add to file', 'Do you want to add this dinner to a new text file?')
+  if add_to_file:
+    file_name = filedialog.asksaveasfilename(defaultextension='.txt')
+    with open(file_name, 'w') as d:
+      d.write(result)
+      d.write('\n')
 
 def delete():
     dinnerentry.delete(0, 'end')
@@ -28,22 +36,30 @@ def delete():
     ingrediententry.delete(0, 'end')
     prepentry.delete(0, 'end')
 
-def option_selected(*args):
-  print(selected.get())
-  
+def dinner_selected(*args):
+  add_to_file = tkinter.messagebox.askyesno('Add to file', 'Would you like to add this dinner to a new text file?')
+  if add_to_file:
+    file_name = filedialog.asksaveasfilename(defaultextension='.txt')
+    with open(file_name, 'w') as d:
+      d.write(selected.get())
+      d.write('\n')  
+
+
+
 pickdinner = Label(root, text="Would you like to pick from a list of dinners? ")
 pickdinner.grid(row=1, columnspan=2)
-selected = StringVar()   
+selected = StringVar() 
+ 
 
 with open('test.txt', 'r') as f:
   options = [line.strip() for line in f]
   
-  
-dropdown = OptionMenu(root, selected, *options)
-dropdown.grid(row=1, column=2)
-    
+selected.set("Dinner options") 
 
-selected.trace("w", option_selected)
+dropdown = OptionMenu(root, selected, *options )
+dropdown.config(width=135)
+dropdown.grid(row=1, column=2)
+selected.trace("w", dinner_selected)
 
 space = Label(root, text="            ")
 space.grid(row=3)
@@ -63,7 +79,7 @@ prep.grid(row=14)
 
 def getvals():
   if not dinnervalue.get() or not typevalue.get() or not ingredientsvalue.get() or not prepvalue.get():
-    messagebox.showerror('Error', 'All fields are required to add a dinner')
+    tkinter.messagebox.showerror('Error', 'All fields are required to add a dinner')
   else:
     with open(t, "a") as f:
       f.write('\n')
@@ -89,14 +105,22 @@ typeentry.grid(row=12, column=1)
 ingrediententry.grid(row=13, column=1)
 prepentry.grid(row=14, column=1)
 
-Button(text="Submit", command=getvals).grid(row=16, column=0)
-Button(text="clear", command=delete).grid(row=16, column=1)
+Button1 = customtkinter.CTkButton(master=root, text="Submit", command=getvals).grid(row=16, column=0)
+Button2 = customtkinter.CTkButton(master=root, text="Clear", command=getvals).grid(row=16, column=1)
+
+space = Label(root, text="")
+space.grid(row=18, columnspan=2)
 
 
-random = Label(root, text="Or would you like me to randomly pick a dinner for you???")
-random.grid(row=20, columnspan=2)
+randomdin = Label(root, text="Or would you like me to randomly pick a dinner for you? or Exit?")
+randomdin.grid(row=20, columnspan=2)
+Button3 = customtkinter.CTkButton(master=root, text="yes", command=randomDinner).grid(row=21, columnspan=1)
 
-Button(text="yes", command=randomDinner).grid(row=21, columnspan=2)
+Button4 = customtkinter.CTkButton(master=root, text="Exit", command=quit_program).grid(row=21, column=1)
+
+
+
+Button5 = customtkinter.CTkButton(master=root, text="All Done", command=quit_program).grid(row=25, columnspan=2)
 
 
 root.mainloop()
